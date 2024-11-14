92faf18761185aef7e7e8beb574ef480662022ed
aliyan-navaid <aliyan.navaid@gmail.com>
2024-11-14 22:08:10 +0500

    added pause menu

--- src/main.asm
+++ src/main.asm
@@ -2,6 +2,13 @@ INCLUDE irvine32.inc
 INCLUDE game.inc
 INCLUDE helper.inc
 
+.DATA
+newline EQU <0Dh, 0Ah>
+pause_iterations EQU 10
+  
+pause_options_msg BYTE "1. Resume", newline
+          BYTE "2. Exit" , newline, 0
+
 .CODE
 
 main PROC
@@ -12,6 +19,7 @@ main PROC
 
     ; Choice: Start Simulation
     cmp eax, 1
+	  MOV ECX, pause_iterations
     JE game_loop
 
     ; Choice: Exit
@@ -27,11 +35,34 @@ main PROC
 
   ; Game loop section
   game_loop:
+    CALL clrscr
+	  
     CALL dumpregs             ; Debugging purpose
     ; CALL initialize_grid
     ; CALL display_grid
     ; CALL update_grid
-    ; JMP game_loop
+
+    MOV EAX, 500
+	  CALL delay
+
+	  CMP ECX, 0
+	  JE pause_menu
+	  DEC ECX
+      JMP game_loop
+
+	  pause_menu:
+		  MOV EDX, OFFSET pause_options_msg
+		  CALL writestring
+		  CALL readint
+
+		CMP EAX, 1
+		JE resume
+		
+		EXIT
+
+		resume:
+			MOV ECX, pause_iterations
+			JMP game_loop
 
   EXIT
 main ENDP
